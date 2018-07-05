e114fb7a29c3d7357056f698b1e568dfe0795a61
Anna Bagrowska <anna_bagrowska@wp.pl>
2018-07-05 20:27:55 +0200

    fix and add style

--- src/presentational/navigation.component.js
+++ src/presentational/navigation.component.js
@@ -9,8 +9,6 @@ const Navigation = props => (
             <div className="container-fluid">
                 <div className="navbar-header">
                     <Link className="navbar-brand" to="/">Państwa.js</Link>
-                </div>
-                <div className="collapse navbar-collapse">
                     <ul className="nav navbar-nav">
                         <li><Link to="/countries">Countries</Link></li>
                         <li><Link to="/continents">Continents</Link></li>
